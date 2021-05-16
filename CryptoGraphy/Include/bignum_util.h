7c10dd2a90e50439fc99392b436243cb0119932e
JunKi, Hong <dylanhong920509@gmail.com>
2021-05-16 16:40:16 +0900

    bignum copy

--- CryptoGraphy/Include/bignum_util.h
+++ CryptoGraphy/Include/bignum_util.h
@@ -4,6 +4,7 @@
 #include "bignum_types.h"
 
 extern int32_t  BN_ucmp(__IN BIGNUM_t* op1, __IN BIGNUM_t * op2);
+extern int32_t  BN_ucpy(__OUT BIGNUM_t* pDst, __IN BIGNUM_t * pSrc);
 extern void     BN_Clear(__INOUT BIGNUM_t* pBN);
 
 #endif  /* !__BIGNUM_UTIL__H__ */
--- CryptoGraphy/Source/bignum_util.c
+++ CryptoGraphy/Source/bignum_util.c
@@ -49,6 +49,39 @@ int32_t BN_ucmp(__IN BIGNUM_t* op1, __IN BIGNUM_t * op2)
     return ret;
 }
 
+int32_t BN_ucpy(__OUT BIGNUM_t* pDst, __IN BIGNUM_t * pSrc)
+{
+    volatile int32_t i, j;
+    int32_t ret;
+
+    if(pDst->capacity >= pSrc->capacity)
+    {
+        ret = 0;
+
+        for(i = 0; i <= pSrc->length; i += 1)
+        {
+            pDst->pWord[i] = pSrc->pWord[i];
+        }
+
+        if(pDst->capacity > pSrc->capacity)
+        {
+            for(j = i; j < pDst->capacity; j += 1)
+            {
+                pDst->pWord[i] = 0U;
+            }
+        }
+
+        pDst->negative = pSrc->negative;
+        pDst->length = pSrc->length;
+    }
+    else
+    {
+        ret = -1;
+    }
+
+    return ret;
+}
+
 void BN_Clear(__INOUT BIGNUM_t* pBN)
 {
     int32_t i;
